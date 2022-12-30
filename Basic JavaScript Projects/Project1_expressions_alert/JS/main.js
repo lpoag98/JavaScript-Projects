74325c482edd38c9b3f67647275fd5f95f344935
Luke Poag <119906569+lpoag98@users.noreply.github.com>
2022-12-30 14:06:31 -0700

    Update main.js

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,13 +1,19 @@
 window.alert('Hello World!') //standard pop up box with ok button
 
 var A = "\"This is a sting!\" Said the coding instructor"; //declaring variables
-document.write(A); 
+document.write(A); //calling for the variables
 
 document.write(". \"This is difficult to figure out\""
 + " -said the person learning coding")
 
 var B = ". \"Nine got eaten? yeah, bet it was that Seven guy.\"" + " -Six "
 document.write(B)
+
+var Sent1 = "\“This is the beginning of the string\”"
+var Sent2 = "\“ and this is the end of the string”"
+var quote1 = " -That guy "
+var quote2 = " -This guy "
+document.write(Sent1+quote1+Sent2+quote2)
 
 var Apple = " Bananas ", Cantaloupe = " Damsom Plum ", Elderberry = " Farkleberry ", Gooseberries = " Hackberry "
 document.write(Gooseberries)
@@ -19,3 +25,10 @@ function add(num1, num2) {
 }
 
 var sum = add(5, 6);
+
+// Function to compute the product of p1 and p2
+function myFunction(p1, p2) {
+    return p1 * p2;
+  }
+
+var result = myFunction(5, 5) 
