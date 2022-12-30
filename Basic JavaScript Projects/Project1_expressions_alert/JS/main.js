968563ae7df84968a337dbb86fcae3b2e496bb02
Luke Poag <119906569+lpoag98@users.noreply.github.com>
2022-12-30 13:38:01 -0700

    Update

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,21 @@
+window.alert('Hello World!') //standard pop up box with ok button
+
+var A = "\"This is a sting!\" Said the coding instructor"; //declaring variables
+document.write(A); 
+
+document.write(". \"This is difficult to figure out\""
++ " -said the person learning coding")
+
+var B = ". \"Nine got eaten? yeah, bet it was that Seven guy.\"" + " -Six "
+document.write(B)
+
+var Apple = " Bananas ", Cantaloupe = " Damsom Plum ", Elderberry = " Farkleberry ", Gooseberries = " Hackberry "
+document.write(Gooseberries)
+
+document.write(3+3);
+
+function add(num1, num2) {
+    return  num1 + num2 ;
+}
+
+var sum = add(5, 6);
