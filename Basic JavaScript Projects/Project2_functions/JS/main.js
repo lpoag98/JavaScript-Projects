3b36252c9e68ed11f3728fdf3b7215df70a98bf0
Luke Poag <119906569+lpoag98@users.noreply.github.com>
2022-12-30 17:39:13 -0700

    update

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,12 @@
+
+  function my_First_Function() {
+    var result = "This is the button text!"
+    
+    document.getElementById("Button_Text").innerHTML = result;
+  }
+
+  function myFunction() {
+    var sentence = "I am learning";
+    sentence += " a lot from this book!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+  }
