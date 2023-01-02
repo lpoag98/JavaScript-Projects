98a6d32fa56b83d7c537fb51bc942987d04d97a7
Luke Poag <119906569+lpoag98@users.noreply.github.com>
2023-01-01 19:21:47 -0700

    dec--add

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,3 +1,4 @@
+  //////////////////////////SIMPLE MATH//////////////////////////
   function addition_Function() {
     var addition = 2 + 2;
     document.getElementById("Math").innerHTML = "2 + 2 = " + addition;
@@ -18,10 +19,17 @@
     document.getElementById("Division").innerHTML = "48 / 6 = " + division;
   }
 
+//////////////////////////SIMPLE MATH//////////////////////////
+
+//////////////////////MULTIPLE OPERATIONS//////////////////////
+
   function more_Math() {
     var simple_Math = 48 / 6;
     document.getElementById("Math2").innerHTML = "1 plus 2, multiplied by 10, divided in half and then subtracted by 5 equals " + simple_Math;
   }
+
+//////////////////////MULTIPLE OPERATIONS//////////////////////
+
 
   function modules_Operator() {
     var simple_Math = 25 % 6;
@@ -33,10 +41,33 @@
     document.getElementById("Math4").innerHTML = -x;
   }
 
-  var A = 5;
-  A++;
+ var A = 5;
+  A++;  //INCRIMENT OPERATOR//
   document.write(A);
+
+  var D = 8;
+D--;  //DECREMENT OPERATOR//
+  document.write(D);
 
   window.alert(Math.random());
 
   window.alert(Math.random() * 100);
+
+/////////////////////////SIMPLE MATH/////////////////////////
+
+
+//////////////////////////DICTIONARY//////////////////////////
+
+  function My_Dictionary() {
+      var Animal = {
+        Species:"Dog",
+        Color:"Black",
+        Breed:"Labrador",
+        Age:"5",
+        Sound:"Bark!"
+      };
+      document.getElementById("Dictionary").innerHTML = Animal.Sound;
+  }
+
+//////////////////////////DICTIONARY//////////////////////////
+
