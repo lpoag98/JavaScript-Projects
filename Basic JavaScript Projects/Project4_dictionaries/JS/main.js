a891f61e3a449177a2850dd5f5dfaaf758b58178
Luke Poag <119906569+lpoag98@users.noreply.github.com>
2023-01-01 23:20:59 -0700

    new folder

---
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -0,0 +1,17 @@
+
+//////////////////////////DICTIONARY//////////////////////////
+
+  function My_Dictionary() {
+      var Animal = {
+        Species:"Dog",
+        Color:"Black",
+        Breed:"Labrador",
+        Age:"5",
+        Sound:"Bark!"
+      };
+      delete Animal.Sound;
+      document.getElementById("Dictionary").innerHTML = Animal.Sound;
+  }
+
+//////////////////////////DICTIONARY//////////////////////////
+
